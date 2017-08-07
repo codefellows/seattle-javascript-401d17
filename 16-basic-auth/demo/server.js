222c224310c1862fdcb98b38914276763f16ab54
bnates <brian.nations@codefellows.com>
2017-08-07 11:58:53 -0700

    added lecture code

---
+++ 16-basic-auth/demo/server.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const express = require('express');
+const debug = require('debug')('cfgram:server');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const cors = require('cors');
+
+const authRouter = require('./route/auth-router.js');
+const errors = require('./lib/error-middleware.js');
+
+dotenv.load();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+mongoose.connect(process.env.MONGODB_URI);
+
+app.use(cors());
+app.use(morgan('dev'));
+
+app.use(authRouter);
+app.use(errors);
+
+app.listen(PORT, () =>  {
+  debug(`currently listening on: ${PORT}`);
+});
---
+++ 16-basic-auth/demo/test/auth-route-test.js
@@ -0,0 +1,72 @@
+'use strict';
+
+const expect = require('chai').expect;
+const request = require('superagent');
+const mongoose = require('mongoose');
+const Promise = require('bluebird');
+const User = require('../model/user.js');
+
+require('../server.js');
+
+const url = `http://localhost:${process.env.PORT}`;
+
+const exampleUser = {
+  username: 'exampleuser',
+  password: '1234',
+  email: 'exampleuser@test.com'
+}
+
+describe('Auth Routes', function() {
+  describe('POST: /api/signup', function() {
+    describe('with a valid body', function() {
+      after( done => {
+        User.remove({})
+        .then( () => done())
+        .catch(done);
+      });
+
+      it('should return a token', done => {
+        request.post(`${url}/api/signup`)
+        .send(exampleUser)
+        .end((err, res) => {
+          if (err) return done(err);
+          console.log('POST: /api/signup TOKEN:', res.text, '\n');
+          expect(res.status).to.equal(200);
+          expect(res.text).to.be.a('string');
+          done();
+        });
+      });
+    });
+  });
+
+  describe('GET: /api/signin', function() {
+    describe('with a valid body', function() {
+      before( done => {
+        let user = new User(exampleUser);
+        user.generatePasswordHash(exampleUser.password)
+        .then( user => user.save())
+        .then( user => {
+          this.tempUser = user;
+          done();
+        });
+      });
+
+      after( done => {
+        User.remove({})
+        .then( () => done())
+        .catch(done);
+      });
+
+      it('should return a token', done => {
+        request.get(`${url}/api/signin`)
+        .auth('exampleuser', '1234')
+        .end((err, res) => {
+          console.log('signed in user:', this.tempUser);
+          console.log('GET: /api/signin TOKEN:', res.text);
+          expect(res.status).to.equal(200);
+          done();
+        });
+      });
+    });
+  });
+});
