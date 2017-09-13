59aaad9d888778bb5d4e950ca8f3f912db42e5b2
bnates <brian.nations@codefellows.com>
2017-09-13 12:22:32 -0700

    added lecture

---
+++ 37-angular_overview/demo/webpack.config.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const HtmlPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: `${__dirname}/app/entry.js`,
+  output: {
+    filename: 'bundle.js',
+    path: `${__dirname}/build`
+  },
+  plugins: [
+    new HtmlPlugin({ template: `${__dirname}/app/index.html` }),
+    new ExtractTextPlugin('bundle.css')
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader"
+        ]
+      }
+    ]
+  } 
+};
